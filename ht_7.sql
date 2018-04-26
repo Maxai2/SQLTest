65f12df5185e17c1b43472c24d7bac939954d79a
Maxai <maxali946@gmail.com>
2018-04-26 07:19:22 +0400

    Update ht_7.sql

--- ht_7.sql
+++ ht_7.sql
@@ -1,3 +1,5 @@
+-- 1. Создать базу данных «Преподаватели» и добавить в неё две таблицы.
+
 --CREATE DATABASE Teachers
 --GO
 --USE Teachers
@@ -6,15 +8,15 @@
 --CREATE TABLE Post
 --(
 --	Id int IDENTITY(1,1),
---	+AFs-Name+AF0- nvarchar(20) NOT NULL,
+--	[Name] nvarchar(20) NOT NULL,
 
---	CONSTRAINT PK+AF8-PostId PRIMARY KEY (Id)
+--	CONSTRAINT PK_PostId PRIMARY KEY (Id)
 --)
 
 --CREATE TABLE Teacher
 --(
 --	Id int IDENTITY(1,1),
---	+AFs-Name+AF0- nvarchar(15) NOT NULL,
+--	[Name] nvarchar(15) NOT NULL,
 --	Code char(10) NOT NULL,
 --	IdPost int NOT NULL,
 --	Tel char(7) NOT NULL,
@@ -22,63 +24,91 @@
 --	Rise numeric(6,2) NOT NULL,
 --	HireDate datetime NOT NULL,
 
---	CONSTRAINT PK+AF8-TeacherId PRIMARY KEY (Id),
---	CONSTRAINT FK+AF8-IdPost FOREIGN KEY (IdPost) REFERENCES Post(Id)
+--	CONSTRAINT PK_TeacherId PRIMARY KEY (Id),
+--	CONSTRAINT FK_IdPost FOREIGN KEY (IdPost) REFERENCES Post(Id)
 --)
 
+-- 2. Удалить таблицу «POST».
+
 --ALTER TABLE Teacher
---DROP CONSTRAINT FK+AF8-IdPost
+--DROP CONSTRAINT FK_IdPost
+
+--DROP TABLE Post
+
+-- 3. В таблице «TEACHER» удалить столбец «IdPost».
 
 --ALTER TABLE Teacher
 --DROP COLUMN IdPost
 
---DROP TABLE Post
+-- 4. Для столбца «HireDate» создать ограничение: дата приёма на работу должна быть не меньше 01.01.1990.
 
 --ALTER TABLE Teacher
---ADD CONSTRAINT CK+AF8-Teacher+AF8-HireDate CHECK(HireDate +AD4- N'19900101')
+--ADD CONSTRAINT CK_Teacher_HireDate CHECK(HireDate > N'19900101')
+
+-- 5. Создать ограничение уникальности для столбца «Code».
 
 --ALTER TABLE Teacher
---ADD CONSTRAINT UC+AF8-TeacherCode UNIQUE (Code)
+--ADD CONSTRAINT UC_TeacherCode UNIQUE (Code)
+
+-- 6. Изменить тип данных в поле Salary c INTEGER на NUMERIC(6,2).
 
 --ALTER TABLE Teacher
 --ALTER COLUMN Salary numeric(6,2) NOT NULL
 
---ALTER TABLE Teacher
---ADD CONSTRAINT CK+AF8-Teacher+AF8-SalaryLim CHECK(1000 +ADwAPQ- Salary AND Salary +ADwAPQ- 5000)
+-- 7. Добавить в таблицу «TEACHER» следующее ограничение: зарплата не должна быть ниже 1000, но и не должна превышать 5000.
 
---EXEC sp+AF8-rename N'Teacher.Tel', N'Phone', N'COLUMN'
+--ALTER TABLE Teacher
+--ADD CONSTRAINT CK_Teacher_SalaryLim CHECK(1000 <= Salary AND Salary <= 5000)
+
+-- 8. Переименовать столбец Tel на Phone.
+
+--EXEC sp_rename N'Teacher.Tel', N'Phone', N'COLUMN'
+
+-- 9. Изменить тип данных в поле Phone с CHAR(7) на CHAR(11).
 
 --ALTER TABLE Teacher
 --ALTER COLUMN Phone char(11) NOT NULL
 
+-- 10. Создать снова таблицу «POST».
+
 --CREATE TABLE Post
 --(
 --	Id int IDENTITY(1,1),
---	+AFs-Name+AF0- nvarchar(20) NOT NULL,
+--	[Name] nvarchar(20) NOT NULL,
 
---	CONSTRAINT PK+AF8-PostId PRIMARY KEY (Id)
+--	CONSTRAINT PK_PostId PRIMARY KEY (Id)
 --)
 
+-- 11. Для поля Name таблицы «POST» задать ограничение на должность (профессор, доцент, преподаватель или ассистент).
+
 --ALTER TABLE Post
---ADD CONSTRAINT CK+AF8-Post+AF8-NameLim CHECK(+AFs-Name+AF0- +AD0- N'+BD8EQAQ+BEQENQRBBEEEPgRA-' OR +AFs-Name+AF0- +AD0- N'+BDQEPgRGBDUEPQRC-' OR +AFs-Name+AF0- +AD0- N'+BD8EQAQ1BD8EPgQ0BDAEMgQwBEIENQQ7BEw-' OR +AFs-Name+AF0- +AD0- N'+BDAEQQRBBDgEQQRCBDUEPQRC-')
+--ADD CONSTRAINT CK_Post_NameLim CHECK([Name] = N'профессор' OR [Name] = N'доцент' OR [Name] = N'преподаватель' OR [Name] = N'ассистент')
+
+-- 12. Для поля Name таблицы «TEACHER» задать ограничение, в котором запретить наличие цифр в фамилии преподавателя.
 
 --ALTER TABLE Teacher
---ADD CONSTRAINT CK+AF8-Post+AF8-NameLimWithNum CHECK(+AFs-Name+AF0- LIKE N'+ACUAWwBe-0-9+AF0AJQ-')
+--ADD CONSTRAINT CK_Post_NameLimWithNum CHECK([Name] LIKE N'%[^0-9]%')
+
+-- 13. Добавить столбец IdPost (int) в таблицу «TEACHER».
 
 --ALTER TABLE Teacher
 --ADD IdPost int NOT NULL
 
+-- 14. Связать поле IdPost таблицы «TEACHER» c полем Id таблицы «POST».
+
 --ALTER TABLE Teacher
---ADD CONSTRAINT FK+AF8-Teacher+AF8-IdPost FOREIGN KEY (IdPost) REFERENCES Post(Id)
+--ADD CONSTRAINT FK_Teacher_IdPost FOREIGN KEY (IdPost) REFERENCES Post(Id)
 
---SET IDENTITY+AF8-INSERT Post ON
+-- 15. Заполнить обе таблицы данными.
 
---INSERT INTO Post(Id,Name) VALUES(1,N'+BB8EQAQ+BEQENQRBBEEEPgRA-')+ADs-
---INSERT INTO post(Id,Name) VALUES(2,N'+BBQEPgRGBDUEPQRC-')+ADs-
---INSERT INTO post(Id,Name) VALUES(3,N'+BB8EQAQ1BD8EPgQ0BDAEMgQwBEIENQQ7BEw-')+ADs-
---INSERT INTO post(Id,Name) VALUES(4,N'+BBAEQQRBBDgEQQRCBDUEPQRC-')+ADs-
+--SET IDENTITY_INSERT Post ON
 
---SET IDENTITY+AF8-INSERT Teacher ON
+--INSERT INTO Post(Id,Name) VALUES(1,N'Профессор');
+--INSERT INTO post(Id,Name) VALUES(2,N'Доцент');
+--INSERT INTO post(Id,Name) VALUES(3,N'Преподаватель');
+--INSERT INTO post(Id,Name) VALUES(4,N'Ассистент');
+
+--SET IDENTITY_INSERT Teacher ON
 
 --ALTER TABLE Teacher
 --ALTER COLUMN Phone char(11) NULL
@@ -86,50 +116,69 @@
 --ALTER TABLE Teacher
 --ALTER COLUMN Code char(10) NULL
 
---INSERT INTO Teacher (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (1, N'+BCEEOAQ0BD4EQAQ+BDI-','0123456789', 1, NULL, 1070, 470, '01.09.1992')+ADs-
---INSERT INTO TEACHER (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (2, N'+BCAEMAQ8BDgESAQ1BDIEQQQ6BDgEOQ-','4567890123', 2, '4567890', 1110, 370, '09.09.1998')+ADs-
---INSERT INTO TEACHER (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (3, N'+BCUEPgRABDUEPQQ6BD4-','1234567890', 3, NULL, 2000, 230, '10.10.2001')+ADs-
---INSERT INTO TEACHER (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (4, N'+BBIEOAQxBEAEPgQyBEEEOgQ4BDk-','2345678901', 4, NULL, 4000, 170, '01.09.2003')+ADs-
---INSERT INTO TEACHER (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (5, N'+BBIEPgRABD4EPwQwBDUEMg-',NULL, 4, NULL, 1500, 150, '02.09.2002')+ADs-
---INSERT INTO TEACHER (Id, +AFs-Name+AF0-, Code, IdPost, phone, Salary, Rise, HireDate)
---VALUES (6, N'+BBoEQwQ3BDgEPQRGBDUEMg-','5678901234', 3, '4567890', 3000, 270, '01.01.1991')+ADs-
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (1, N'Сидоров','0123456789', 1, NULL, 1070, 470, '01.09.1992');
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (2, N'Рамишевский','4567890123', 2, '4567890', 1110, 370, '09.09.1998');
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (3, N'Хоренко','1234567890', 3, NULL, 2000, 230, '10.10.2001');
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (4, N'Вибровский','2345678901', 4, NULL, 4000, 170, '01.09.2003');
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (5, N'Воропаев',NULL, 4, NULL, 1500, 150, '02.09.2002');
+--INSERT INTO TEACHER (Id, Name, Code, IdPost, phone, Salary, Rise, HireDate)
+--VALUES (6, N'Кузинцев','5678901234', 3, '4567890', 3000, 270, '01.01.1991');
+
+/*16. Cоздать представления:
+
+16.1. Все названия должностей.*/
 
 --CREATE VIEW PostName
 --AS
---SELECT Post.+AFs-Name+AF0-
+--SELECT Post.[Name]
 --FROM Post
+
+/*16.2. Все фамилии учителей. */
 
 --CREATE VIEW TeachersLastName
 --AS
---SELECT Teacher.+AFs-Name+AF0-
+--SELECT Teacher.[Name]
 --FROM Teacher
 
---CREATE VIEW IdNamePostSal ??
+/*16.3. Идентификатор, фамилия учителя, его должность, общая з\п (сортировать по з\п). ? */
+
+--CREATE VIEW IdNamePostSal 
 --AS
---SELECT TOP 100 PERCENT Teacher.Id, Teacher.+AFs-Name+AF0-, Post.+AFs-Name+AF0- AS PostName, Teacher.Salary
+--SELECT Teacher.Id, Teacher.[Name], Post.[Name] AS PostName, Teacher.Salary AS Salary
 --FROM Teacher JOIN Post
---	ON Teacher.IdPost +AD0- Post.Id
---ORDER BY Teacher.Salary
+--	ON Teacher.IdPost = Post.Id
+
+--SELECT *
+--FROM IdNamePostSal
+--ORDER BY Salary
+
+/*16.4. Идентификационный номер, фамилия, номер телефона (только те, у кого есть номер телефона).*/
 
 --CREATE VIEW IdNamePhone
 --AS
---SELECT Teacher.Id, Teacher.+AFs-Name+AF0-, Teacher.Phone
+--SELECT Teacher.Id, Teacher.[Name], Teacher.Phone
 --FROM Teacher
 --WHERE Teacher.Phone IS NOT NULL
 
+
+/*16.5. Фамилия, должность, дата приема в формате дд/мм/гг. */
+
 --CREATE VIEW NamePostDateForm
 --AS
---SELECT Teacher.+AFs-Name+AF0-, Post.+AFs-Name+AF0- AS PostName, FORMAT(Teacher.HireDate, N'dd/MM/yy') AS HireDate
+--SELECT Teacher.[Name], Post.[Name] AS PostName, FORMAT(Teacher.HireDate, N'dd/MM/yy') AS HireDate
 --FROM Teacher JOIN Post
---	ON Teacher.IdPost +AD0- Post.Id
+--	ON Teacher.IdPost = Post.Id
+
+
+/* 16.6. Фамилия, должность, дата приема в формате дд месяц_текстом гггг.*/
 
 --CREATE VIEW NamePostDateFormMonthName
 --AS
---SELECT Teacher.+AFs-Name+AF0-, Post.+AFs-Name+AF0- AS PostName, FORMAT(Teacher.HireDate, N'dd/MMM/yy') AS HireDate
+--SELECT Teacher.[Name], Post.[Name] AS PostName, FORMAT(Teacher.HireDate, N'dd/MMM/yy') AS HireDate
 --FROM Teacher JOIN Post
---	ON Teacher.IdPost +AD0- Post.Id
+--	ON Teacher.IdPost = Post.Id
