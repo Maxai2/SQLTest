1a71fbd012d675a8b70c01c2a81eb25fd25e12a0
Maxai <maxali946@gmail.com>
2018-04-30 11:52:46 +0400

    Update ht_8.sql

--- ht_8.sql
+++ ht_8.sql
@@ -84,7 +84,7 @@
 --	ON Books.Id_Category = Categories.Id
 --ORDER BY Themes.Id
 
-/*4. ??? ХП “Добавление студента”. ХП добавляет студента и группу. ХП принимает имя, фамилию студента и название группы. 
+/*4. ХП “Добавление студента”. ХП добавляет студента и группу. ХП принимает имя, фамилию студента и название группы. 
 Если группа с таким названием существует, то в Id_Group указываете Id этой группы. 
 Если группы с таким именем не существует, то сначала добавляем группу, а потом студента. 
 Обратите внимание, что названия групп хранятся в верхнем регистре, но никто не гарантирует, 
@@ -99,7 +99,7 @@
 	
 --	DECLARE @IdGroup int = 0;
 
---	IF (@GroupName = (SELECT Groups.[Name] FROM Groups))
+--	IF (@GroupName = ANY (SELECT Groups.[Name] FROM Groups))
 --	BEGIN
 --		SELECT @IdGroup = Groups.Id
 --		FROM Groups
@@ -135,32 +135,31 @@
 /*5. ХП “Закупка популярных книг”. ХП выбирает топ-5 самых популярных книг (среди студентов и преподавателей одновременно)
  и покупает еще по 3 экземпляра книги. */
 
-ALTER PROCEDURE ByePop5Books
-AS
-BEGIN
-	UPDATE Books
-	SET Books.Quantity += 0
-	WHERE Books.Id IN
-	(
-		SELECT TOP(5) *
-		FROM (SELECT *
-		FROM (SELECT COUNT(S_Cards.Id_Book) AS BooksCount, Books.[Name], Books.Id
-		FROM S_Cards JOIN Books
-			ON S_Cards.Id_Book = Books.Id
-		GROUP BY Books.[Name], Books.Id) AS Stud
-		UNION
-		SELECT *
-		FROM (SELECT COUNT(T_Cards.Id_Book) AS BooksCount, Books.[Name], Books.Id
-		FROM T_Cards JOIN Books
-			ON T_Cards.Id_Book = Books.Id
-		GROUP BY Books.[Name], Books.Id) AS Teach) AS TempTab
-		ORDER BY TempTab.BooksCount DESC
-	) 
-END
+--ALTER PROCEDURE ByeTop5Books
+--AS
+--BEGIN
+--	UPDATE Books
+--	SET Books.Quantity += 0
+--	WHERE Books.Id IN
+--	(
+--		SELECT *
+--		FROM (SELECT COUNT(S_Cards.Id_Book) AS BooksCount, Books.[Name], Books.Id, N'Students' AS Team
+--		FROM S_Cards JOIN Books
+--			ON S_Cards.Id_Book = Books.Id
+--		GROUP BY Books.[Name], Books.Id) AS Stud
+--		UNION
+--		SELECT *
+--		FROM (SELECT COUNT(T_Cards.Id_Book) AS BooksCount, Books.[Name], Books.Id, N'Teachers' AS Team
+--		FROM T_Cards JOIN Books
+--			ON T_Cards.Id_Book = Books.Id
+--		GROUP BY Books.[Name], Books.Id) AS Teach
+--		ORDER BY Team
+--	) 
+--END
 
-SELECT Books.[Name], Books.Quantity
-FROM Books
-ORDER BY Books.Quantity DESC
+--SELECT Books.[Name], Books.Quantity
+--FROM Books
+--ORDER BY Books.Quantity DESC
 
 /*6. ХП “Избавление от непопулярных книг”. ХП выбирает топ-5 не популярных книг и отдает половину другому учебному заведению. */
 
@@ -169,11 +168,54 @@ ORDER BY Books.Quantity DESC
 /*7. ХП “Студент берет книгу”. ХП получает Id студента и Id книги, проверяет если количество книг больше нуля, 
 тогда выдает студенту книгу. При выдаче студенту книги необходимо уменьшать ее количество в библиотеки. Перед тем как
 выдавать книгу проверяем сколько уже книг на руках у этого студента. Если 3-4
-книги, то выводим предупреждение, а если уже 5 книг, то не выдаем ему новую книгу.
+книги, то выводим предупреждение, а если уже 5 книг, то не выдаем ему новую книгу. */
 
+CREATE PROCEDURE StudTakeBook
+	@StudId int,
+	@BookId int
+AS
+BEGIN
+	DECLARE @BookCount int = 0;
+	SELECT @BookCount = COUNT(*)
+	FROM Books
+	WHERE Books.Id = @BookId
 
+	IF @BookCount > 0
+	BEGIN
 
-8. ХП “Преподаватель берет книгу”.
+		DECLARE	@StudBookCount int = 0;
+		SELECT @StudBookCount = COUNT(*)
+		FROM S_Cards
+		WHERE S_Cards.Id_Student = @StudId
+		
+		DECLARE @StudName nvarchar(60);
+		SELECT @StudName = Students.FirstName + ' ' + Students.LastName 
+		FROM Students
+
+		IF (@StudBookCount = 3) OR (@StudBookCount = 4)
+		BEGIN
+			PRINT @StudName + N' has ' + CAST(@StudBookCount, nvarchar(5)) + N' books.'
+
+			DECLARE @LastIdS_Card int = 0;
+			SELECT TOP(1) @LastIdS_Card = Groups.Id + 1
+			FROM Groups
+			ORDER BY Groups.Id DESC
+
+			INSERT INTO S_Cards (Id, Id_Student, Id_Book, DateOut, DateIn, Id_Lib)
+			VALUES ()
+
+			UPDATE Books
+			SET Books.Quantity -= 1
+			WHERE Books.Id = @BookId
+		END
+		ELSE
+		IF @StudBookCount = 5
+			PRINT @StudName + N' has ' + CAST(@StudBookCount, nvarchar(5)) + N' books. You exceeded the limit.'
+	END
+
+END
+
+/*8. ХП “Преподаватель берет книгу”.
 9. ХП “Студент возвращает книгу”. ХП получает Id студента и Id книги. В таблицу
 S_Cards заносится информация о возвращении книги. Если студент держал у
 себя книгу больше года, то ему выписывается штраф.
