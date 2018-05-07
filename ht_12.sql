262e0319c4e71a6f931f546e24d3bd77411bc40c
Maxai <maxali946@gmail.com>
2018-05-07 11:31:11 +0400

    Update ht_12.sql

--- ht_12.sql
+++ ht_12.sql
@@ -1,132 +1,174 @@
 /*Реализовать триггеры таким образом, чтоб выполнялись следующие требования. 
-1. Нельзя было выдавать книгу, которой уже нет в библиотеке (по количеству).
-2. При возврате определенной книги, её количество должно увеличиваться. 
-3. При выдаче книги, ее количество должно уменьшаться.*/
+1. Нельзя было выдавать книгу, которой уже нет в библиотеке (по количеству). */
+/*3. При выдаче книги, ее количество должно уменьшаться.*/
 
---CREATE TRIGGER CheckBookGiveStud 
---ON S_Cards AFTER INSERT, UPDATE
---AS
---BEGIN
---	IF UPDATE(DateIn)
---	BEGIN
---		UPDATE Books
---		SET Quantity += 1
---		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
---		RETURN;
---	END
---	ELSE
---	IF UPDATE(DateOut)
---		BEGIN
---		UPDATE Books
---		SET Quantity -= 1
---		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
---		RETURN;
---	END
+CREATE TRIGGER CheckBookGiveStud 
+ON S_Cards AFTER INSERT
+AS
+BEGIN
+	DECLARE @bookQuant int = 0;
+	SELECT @bookQuant = Books.Quantity
+	FROM Books JOIN inserted
+		ON Books.Id = inserted.Id_Book
+	WHERE inserted.Id_Book = Books.Id
 
---	DECLARE @bookQuant int = 0;
---	SELECT @bookQuant = Books.Quantity
---	FROM Books JOIN inserted
---		ON Books.Id = inserted.Id_Book
---	WHERE inserted.Id_Book = Books.Id
+	DECLARE @bookName nvarchar(100);
+	SELECT @bookName = Books.[Name] 
+	FROM Books JOIN inserted 
+		ON Books.Id = inserted.Id_Book 
+	WHERE inserted.Id_Book = Books.Id
 
---	DECLARE @bookName nvarchar(100);
---	SELECT @bookName = Books.[Name] 
---	FROM Books JOIN inserted 
---		ON Books.Id = inserted.Id_Book 
---	WHERE inserted.Id_Book = Books.Id
+	IF @bookQuant = 0
+	BEGIN
+		PRINT 'No more (' + @bookName + ') left in the library!'
+		ROLLBACK TRAN;
+	END
+	ELSE
+	BEGIN
+		PRINT 'Take please!'
+		UPDATE Books
+		SET Quantity -= 1
+		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
+	END
+END
 
---	IF @bookQuant = 0
---	BEGIN
---		PRINT 'No more (' + @bookName + ') left in the library!'
---		ROLLBACK TRAN;
---	END
---	ELSE
---		PRINT 'Take please!'
---END
+---------------------------------------------------------------------------------
 
-----------------------------------------------------------------------------
+CREATE TRIGGER CheckBookGiveTeach 
+ON T_Cards AFTER INSERT
+AS
+BEGIN
+	DECLARE @bookQuant int = 0;
+	SELECT @bookQuant = Books.Quantity
+	FROM Books JOIN inserted
+		ON Books.Id = inserted.Id_Book
+	WHERE inserted.Id_Book = Books.Id
 
---CREATE TRIGGER CheckBookGiveTeach 
---ON T_Cards AFTER INSERT, UPDATE
---AS
---BEGIN
---	IF UPDATE(DateIn)
---	BEGIN
---		UPDATE Books
---		SET Quantity += 1
---		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
---		RETURN;
---	END
---	ELSE
---	IF UPDATE(DateOut)
---		BEGIN
---		UPDATE Books
---		SET Quantity -= 1
---		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
---		RETURN;
---	END
+	DECLARE @bookName nvarchar(100);
+	SELECT @bookName = Books.[Name] 
+	FROM Books JOIN inserted 
+		ON Books.Id = inserted.Id_Book 
+	WHERE inserted.Id_Book = Books.Id
 
---	DECLARE @bookQuant int = 0;
---	SELECT @bookQuant = Books.Quantity
---	FROM Books JOIN inserted
---		ON Books.Id = inserted.Id_Book
---	WHERE inserted.Id_Book = Books.Id
+	IF @bookQuant = 0
+	BEGIN
+		PRINT 'No more (' + @bookName + ') left in the library!'
+		ROLLBACK TRAN;
+	END
+	ELSE
+	BEGIN
+		PRINT 'Take please!'
+		UPDATE Books
+		SET Quantity -= 1
+		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
+	END
+END
 
---	DECLARE @bookName nvarchar(100);
---	SELECT @bookName = Books.[Name] 
---	FROM Books JOIN inserted 
---		ON Books.Id = inserted.Id_Book 
---	WHERE inserted.Id_Book = Books.Id
+-----------------------------------------------------------------
 
---	IF @bookQuant = 0
---	BEGIN
---		PRINT 'No more (' + @bookName + ') left in the library!'
---		ROLLBACK TRAN;
---	END
---	ELSE
---		PRINT 'Take please!'
---END
+/*2. При возврате определенной книги, её количество должно увеличиваться. */
+
+CREATE TRIGGER BackBookStud
+ON S_Cards AFTER UPDATE
+AS
+BEGIN
+	IF UPDATE(DateIn)
+	BEGIN
+		UPDATE Books
+		SET Quantity += 1
+		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
+		PRINT N'Quan+1'
+		RETURN;
+	END
+END
+
+-------------------------------------------------------------------
+
+CREATE TRIGGER BackBookTeach
+ON T_Cards AFTER UPDATE
+AS
+BEGIN
+	IF UPDATE(DateIn)
+	BEGIN
+		UPDATE Books
+		SET Quantity += 1
+		WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
+		PRINT N'Quan+1'
+		RETURN;
+	END
+END
+
+----------------------------------------------------------------
+
+--UPDATE S_Cards
+--SET S_Cards.DateIn = GETDATE()
+--WHERE S_Cards.Id = 11
 
 --INSERT INTO S_Cards (Id, Id_Student, Id_Lib, Id_Book, DateOut, DateIn)
---VALUES (13, 6, 1, 6, GETDATE(), NULL)
+--VALUES (132, 6, 1, 14, GETDATE(), NULL)
+
+--SELECT *
+--FROM S_Cards
 
 --SELECT *
 --FROM Books
 
+--UPDATE Books
+--SET Books.Quantity -= 1
+
+----------------------------------------------------------------------------
+
 /*4. Нельзя выдать более трёх книг одному студенту на руки.*/
 
---CREATE TRIGGER NoMoreThanThree
---ON S_Cards AFTER INSERT
---AS
---BEGIN
---	DECLARE @curStudBookCount int = 0;
---	SELECT @curStudBookCount = COUNT(inserted.Id_Student)
---	FROM inserted JOIN Students
---		ON inserted.Id_Student = Students.Id
---	WHERE Students.Id = inserted.Id_Student
+CREATE TRIGGER NoMoreThanThree
+ON S_Cards AFTER INSERT
+AS
+BEGIN
+	DECLARE @curStudBookCount int = 0;
+	SELECT @curStudBookCount = COUNT(inserted.Id_Student)
+	FROM inserted JOIN Students
+		ON inserted.Id_Student = Students.Id
+	WHERE Students.Id = inserted.Id_Student
 
---	IF @curStudBookCount > 3
---	BEGIN
---		PRINT N'You have 3 books no longer issued!'
---		ROLLBACK TRAN
---	END
---	ELSE
---		PRINT N'Take please!'
---END
+	IF @curStudBookCount > 3
+	BEGIN
+		PRINT N'You have 3 books no longer issued!'
+		ROLLBACK TRAN
+	END
+	ELSE
+		PRINT N'Take please!'
+END
 
 /*5. Нельзя выдавать новую книгу студенту, если он сейчас читает хоть одну книгу дольше 2 месяцев. */
 
---CREATE TRIGGER TwoMonthMore
---ON S_Cards AFTER INSERT
---AS
---BEGIN
---	DECLARE @monthCount int = 0;
---	SELECT @monthCount = MONTH(GETDATE()) - (SELECT MONTH(inserted.DateOut) FROM inserted)
---END
+CREATE TRIGGER TwoMonthMore
+ON S_Cards AFTER INSERT
+AS
+BEGIN
+	IF (YEAR(GETDATE()) - (SELECT TOP(1) YEAR(inserted.DateOut) FROM inserted)) = 0
+	BEGIN
+		DECLARE @monthCount int = 0;
+		SELECT @monthCount = MONTH(GETDATE()) - (SELECT TOP(1) MONTH(inserted.DateOut) FROM inserted)
 
---DECLARE @monthCount int = 0;
---SELECT @monthCount = MONTH(GETDATE()) - (SELECT TOP(1) MONTH(S_Cards.DateOut) FROM S_Cards WHERE S_Cards.Id_Student = 2)
---PRINT @monthCount
+		IF @monthCount > 2
+		BEGIN
+			PRINT N'You have exceeded the storage limit of the book by month!'
+			ROLLBACK TRAN
+			RETURN
+		END
+		ELSE
+		BEGIN
+			PRINT 'Take please!'
+			UPDATE Books
+			SET Quantity -= 1
+			WHERE Books.Id = ANY (SELECT inserted.Id_Book FROM inserted)
+		END
+	END
+	
+	PRINT N'You have exceeded the storage limit of the book by year!'
+	ROLLBACK TRAN
+
+END
 
 /*6. При удалении книги, данные о ней должны копироваться в таблицу LibDeleted. */
 
@@ -148,11 +190,15 @@ CREATE TRIGGER OnDeleteCopy
 ON Books AFTER DELETE
 AS
 BEGIN
+	SELECT *
+	FROM deleted
+	
 	INSERT INTO LibDeleted ([Name], Pages, YearPress, Id_Themes, Id_Category, Id_Author, Id_Press, Comment, Quantity)
-	VALUE (deleted.[Name], deleted.Pages, deleted.YearPress, deleted.Id_Themes, deleted.Id_Category, deleted.Id_Author, deleted.Id_Press, deleted.Comment, deleted.Quantity)
+	SELECT deleted.[Name], deleted.Pages, deleted.YearPress, deleted.Id_Themes, deleted.Id_Category, deleted.Id_Author, deleted.Id_Press, deleted.Comment, deleted.Quantity
+	FROM deleted
 END
 
-SELECT *
-FROM Books
+--SELECT *
+--FROM Books
 
 /*(!) Обратите внимание, что первые 3 пункта относятся к выдаче книг и студентам и учителям.*/
