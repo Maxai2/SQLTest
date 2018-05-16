9fc83cd74f7e619c1bddcf2c960812985e07fa1d
Хамелеон <maxali946@gmail.com>
2018-05-16 15:54:55 +0400

    Revert "Update BackupRestore.sql"
    This reverts commit 6faad4b1b00bd1ae98a7d05b8e1d55f3efc4a236.

--- BackupRestore.sql
+++ BackupRestore.sql
@@ -1,123 +1,53 @@
-CREATE DATABASE Turisma_ru
-GO
-USE Turisma_ru
-GO
-
-BACKUP DATABASE Tourism
-TO DISK = ''
+BACKUP DATABASE Library_ru
+TO DISK = 'C:\TempSQL\Lib_ru.bak'
 WITH
-NAME = 'First BACKUP(full)',
-DESCRIPTION = 'Create DATABASE'
+NAME = 'Full backup',
+DESCRIPTION = 'First backup of Library_ru'
 
+RESTORE HEADERONLY
+FROM DISK = 'C:\TempSQL\Lib_ru.bak'
+
+DROP DATABASE Library_ru
+
+RESTORE DATABASE Library_ru
+FROM DISK = 'C:\TempSQL\Lib_ru.bak'
+
+CREATE TABLE MyTable
+(
+	Id int PRIMARY KEY IDENTITY(1, 1),
+	SomeText nvarchar(50) NOT NULL
+)
 GO
-CREATE TABLE Country
-(
-	Id int IDENTITY(1, 1),
-	Country_Name nvarchar(25) NOT NULL,
+INSERT INTO MyTable (SomeText)
+VALUES (N'qwerqwer'),
+	(N'asdfasdf'),
+	(N'asgergdfgasdfgf'),
+	(N'asdfgdfg')
 
-	CONSTRAINT PK_Country_Id PRIMARY KEY (Id)
-)
+SELECT *
+FROM MyTable
 
-CREATE TABLE City
-(
-	Id int IDENTITY(1, 1),
-	City_Name nvarchar(30) NOT NULL,
-	Country_Id int NOT NULL,
-
-	CONSTRAINT PK_City_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Country_Id FOREIGN KEY (Country_Id) REFERENCES Country(Id)
-)
-
-CREATE TABLE [User]
-(
-	Id int IDENTITY(1, 1),
-	First_Name nvarchar(20) NOT NULL,
-	Last_Name nvarchar(20) NOT NULL,
-	Picture_Src nvarchar(50) NULL,
-	Mail nvarchar(20) NULL,
-	FB nvarchar(20) NULL,
-
-	CONSTRAINT PK_User_Id PRIMARY KEY (Id)
-)
-
-CREATE TABLE [Route]
-(
-	Id int IDENTITY(1, 1),
-	Route_Name nvarchar(50) NOT NULL,
-	Route_Description text NULL,
-	Route_Time time NOT NULL,
-	City_Id int NOT NULL,
-	[User_Id] int NOT NULL,
-
-	CONSTRAINT PK_Route_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Route_City_Id FOREIGN KEY (City_Id) REFERENCES City(Id),
-	CONSTRAINT FK_Route_User_Id FOREIGN KEY ([User_Id]) REFERENCES [User](Id)
-)
-
-CREATE TABLE Route_Type
-(
-	Id int IDENTITY(1, 1),
-	Route_Type_Name nvarchar(50) NOT NULL,
-	Route_Id int NOT NULL,
-
-	CONSTRAINT PK_Route_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Route_Type_Route_Id FOREIGN KEY (Route_Id) REFERENCES [Route](Id)
-)
-
-CREATE TABLE Point
-(
-	Id int IDENTITY(1, 1),
-	Point_Name nvarchar(50) NOT NULL,
-	Point_Location geography NOT NULL,
-	Point_Description text NULL,
-	Picture_Src nvarchar(50) NULL,
-	[Index] int NOT NULL,
-	Route_Id int NOT NULL,
-
-	CONSTRAINT PK_Point_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Point_Route_Id FOREIGN KEY (Route_Id) REFERENCES [Route](Id)
-)
-
-CREATE TABLE Raiting
-(
-	Id int IDENTITY(1, 1),
-	[Date] date NOT NULL,
-	Mark int NOT NULL,
-	Route_Id int NOT NULL,
-	[User_Id] int NOT NULL,
-
-	CONSTRAINT PK_Raiting_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Raiting_Route_Id FOREIGN KEY (Route_Id) REFERENCES [Route](Id),
-	CONSTRAINT FK_RAINTING_User_Id FOREIGN KEY ([User_Id]) REFERENCES [User](Id)
-)
-
-CREATE TABLE Comment
-(
-	Id int IDENTITY(1, 1),
-	[Date] date NOT NULL,
-	[Text] text NOT NULL,
-	Like_Count int NOT NULL DEFAULT(0),
-	Route_Id int NOT NULL,
-	[User_Id] int NOT NULL,
-
-	CONSTRAINT PK_Comment_Id PRIMARY KEY (Id),
-	CONSTRAINT FK_Comment_Route_Id FOREIGN KEY (Route_Id) REFERENCES [Route](Id),
-	CONSTRAINT FK_Comment_User_Id FOREIGN KEY ([User_Id]) REFERENCES [User](Id)
-)
-
+USE [master]
 GO
-
-BACKUP DATABASE Tourism
-TO DISK = ''
+BACKUP DATABASE Library_ru
+TO DISK = 'C:\TempSQL\Lib_ru.bak'
 WITH
-NAME = 'Dif BackUp',
-DESCRIPTION = 'Create Tables',
+NAME = 'Differential backup',
+DESCRIPTION = 'Second backup of Library_ru',
 DIFFERENTIAL
 
-GO
---------------------------------------------------------------------------------------
+RESTORE DATABASE Library_ru
+FROM DISK = 'C:\TempSQL\Lib_ru.bak'
+WITH
+NORECOVERY
 
-SELECT [Route].Route_Name, City.City_Name
-FROM [Route] JOIN City
-	ON [Route].City_Id = City.Id
-WHERE City_Name = ''
+RESTORE DATABASE Library_ru
+FROM DISK = 'C:\TempSQL\Lib_ru.bak'
+WITH
+FILE = 2,
+RECOVERY
+
+USE Library_ru
+GO
+SELECT * 
+FROM MyTable
